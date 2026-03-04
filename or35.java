d527916827ea0302155a9a18dd47956b23ce1926
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-04 07:46:34 +0400

    or35

---
+++ or35.java
@@ -0,0 +1,12 @@
+public class or35 {
+    public static void main(String[] args) {
+        
+    }
+    public boolean or35_(int n) {
+        if (n%3 == 0 || n%5 == 0){
+            return true;
+        }
+        return false;
+    }
+
+}
