3d41c727a23f3fad93adc0ad06be99504c15c02e
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-04 08:15:23 +0400

    in3050

---
+++ in3050.java
@@ -0,0 +1,9 @@
+public class in3050 {
+    public static void main(String[] args) {
+        
+    }
+    public boolean in3050_(int a, int b) {
+        return ((a>29&&a<41)&&(b>29&&b<41)||(a>39&&a<51)&&(b>39&&b<51));
+    }
+
+}
