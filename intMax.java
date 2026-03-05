63352cff0e41f651dc01871e303e86d2d91d0f86
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-05 08:13:51 +0400

    public int intMax(int a, int b, int c) {   if (a>b && a>c){     return a;   }   if (b>a && b>c){     return b;   }   return c; }

---
+++ intMax.java
@@ -0,0 +1,14 @@
+public class intMax {
+    public static void main(String[] args) {
+        
+    }
+    public int intMax_(int a, int b, int c) {
+        if (a>b && a>c){
+            return a;
+        }
+        if (b>a && b>c){
+            return b;
+        }
+        return c;
+    }
+}
