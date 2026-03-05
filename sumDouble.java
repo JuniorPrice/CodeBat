1366f4ecc990b502395660973d01173bc59250de
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-05 07:29:38 +0400

    sumDouble

---
+++ sumDouble.java
@@ -0,0 +1,12 @@
+public class sumDouble {
+    public static void main(String[] args) {
+        
+    }
+    public int sumDouble(int a, int b) {
+        if (a == b){
+            return 2*(a+b);
+        }
+        return a+b;
+    }
+
+}
