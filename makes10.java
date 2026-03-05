cf3555e2050dd4603c8bcb34020385b87374918a
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-05 07:34:18 +0400

    makes10

---
+++ makes10.java
@@ -0,0 +1,9 @@
+public class makes10 {
+    public static void main(String[] args) {
+        
+    }
+    public boolean makes10_(int a, int b) {
+        return a==10 || b==10 || (a+b) ==10;
+    }
+
+}
--- sumDouble.java
+++ sumDouble.java
@@ -2,7 +2,7 @@ public class sumDouble {
     public static void main(String[] args) {
         
     }
-    public int sumDouble(int a, int b) {
+    public int sumDouble_(int a, int b) {
         if (a == b){
             return 2*(a+b);
         }
