bbc91944f4bbd836a4ddc383069269e1cc0c2599
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-29 11:35:39 +0400

    comboString

---
+++ string_1/comboString.java
@@ -0,0 +1,11 @@
+public class comboString {
+    public static void main(String[] args) {
+        
+    }
+    public String comboString_(String a, String b) {
+        if (a.length() > b.length()){
+            return b+ a+ b;
+        }
+        return a+ b+ a;
+    }
+}
