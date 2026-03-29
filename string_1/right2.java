41cf2db48a69e12ee2ca8a3d1c87b7e962b1f886
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-29 11:46:48 +0400

    right2

---
+++ string_1/right2.java
@@ -0,0 +1,8 @@
+public class right2 {
+    public static void main(String[] args) {
+        
+    }
+    public String right2_(String str) {
+        return str.substring(str.length()-2)+ str.substring(0,str.length()-2);
+    }
+}
