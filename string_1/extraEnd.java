8bc5492af080601974dfe9cb803e5caf38e56249
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-29 11:39:36 +0400

    nonStart

--- string_1/extraEnd.java
+++ string_1/extraEnd.java
@@ -2,7 +2,7 @@ public class extraEnd {
     public static void main(String[] args) {
         
     }
-    public String extraEnd(String str) {
+    public String extraEnd_(String str) {
         return str.substring(str.length()-2) + str.substring(str.length()-2) + str.substring(str.length()-2);
     }
 }
---
+++ string_1/nonStart.java
@@ -0,0 +1,8 @@
+public class nonStart {
+    public static void main(String[] args) {
+        
+    }
+    public String nonStart_(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
+}
