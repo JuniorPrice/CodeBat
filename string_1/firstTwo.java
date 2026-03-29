10b5a2f656e1772998e87619bf689e32cc841027
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-29 11:26:59 +0400

    firstTwo

---
+++ string_1/firstTwo.java
@@ -0,0 +1,11 @@
+public class firstTwo {
+    public static void main(String[] args) {
+        
+    }
+    public String firstTwo_(String str) {
+        if (str.length() > 1){
+            return str.substring(0,2);
+        }
+        return str;
+    }
+}
