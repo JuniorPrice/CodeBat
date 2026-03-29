b9cc08036c6a458b5e6e46aa1ac804feb6110d0e
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-29 11:29:52 +0400

    firstHalf

---
+++ string_1/firstHalf.java
@@ -0,0 +1,8 @@
+public class firstHalf {
+    public static void main(String[] args) {
+        
+    }
+    public String firstHalf_(String str) {
+        return str.substring(0,str.length()/2);
+    }
+}
