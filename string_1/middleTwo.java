7c0a2616404a2a804f268471cd972e8a34b13178
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-29 12:24:08 +0400

    middleTwo

---
+++ string_1/middleTwo.java
@@ -0,0 +1,8 @@
+public class middleTwo {
+    public static void main(String[] args) {
+        
+    }
+    public String middleTwo_(String str) {
+        return str.substring((str.length()/2)-1, (str.length()/2)+1);
+    }
+}
