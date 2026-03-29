46214a2dccce0b5c780754fc91e2ccd05ed96dcf
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-29 08:48:29 +0400

    makeOutWord

---
+++ string_1/makeOutWord.java
@@ -0,0 +1,9 @@
+
+public class makeOutWord {
+    public static void main(String[] args) {
+        
+    }
+    public String makeOutWord_(String out, String word) {
+        return out.substring(0,2)+ word+ out.substring(2);
+    }
+}
