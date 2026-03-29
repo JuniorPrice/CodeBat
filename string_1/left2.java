9dce0c8bea42ba1fbcb9f25e4ba095e8f6bc00ea
Ya'Rab Al-Mamari <yarop123456789@gmail.com>
2026-03-29 11:42:53 +0400

    left2

---
+++ string_1/left2.java
@@ -0,0 +1,8 @@
+public class left2 {
+    public static void main(String[] args) {
+        
+    }
+    public String left2_(String str) {
+        return str.substring(2) + str.substring(0,2);
+    }
+}
